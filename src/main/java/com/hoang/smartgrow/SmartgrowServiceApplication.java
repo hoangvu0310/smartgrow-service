41c92995caa6747347d980b5209d1615a255c5a0
hoangvu0310 <hoangvu031003@gmail.com>
2025-12-30 14:36:54 +0700

    init project base

---
+++ src/main/java/com/hoang/smartgrow/SmartgrowServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hoang.smartgrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartgrowServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartgrowServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/hoang/smartgrow/SmartgrowServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoang.smartgrow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartgrowServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
